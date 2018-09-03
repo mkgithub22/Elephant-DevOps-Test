82962c3899cc8217747df2b351abe2edfe655b41
mkgithub22 <36643416+mkgithub22@users.noreply.github.com>
2018-09-03 18:30:45 -0400

    Update build.sh
    check for customized web page, and copy the index file over

--- build.sh
+++ build.sh
@@ -26,3 +26,25 @@ else
   # todo: check return code
 fi
 
+# test to see if our customized web page is in place
+curl http://localhost | grep "Automation for the People"
+ourWebPage=$?
+
+if test $ourWebPage -eq 0
+then
+  echo "Our web page is set up.  All done"
+else
+  # most likely the default apache web site is in place
+  curl http://localhost | grep "Test Page for the Apache HTTP Server"
+  defaultWebPage=$?
+  if test $defaultWebPage -eq 0
+    then
+    # create our web page and copy it to the apache folder
+    echo "Default web page is set up.  Adding our index.html"
+    echo "<html><title>Elephant DevOps Test</title><p>Automation for the People</html>" >index2.html
+    sudo mv /usr/share/httpd/noindex/index.html /usr/share/httpd/noindex/index.html.bak
+    sudo cp index.html /usr/share/httpd/noindex/index.html
+  else
+    echo "Unexpected web page result"
+  fi
+fi
